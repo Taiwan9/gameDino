484f9075d5d7ee54435108635fc8d24b66eff585
Taiwan9 <clownser1994@gmail.com>
2022-04-20 17:09:51 -0300

    cactus move and random created cactus

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function jump() {
     isJumping = true
 
     let upInterval = setInterval(() => {
-        if (position >= 180) {
+        if (position >= 150) {
             clearInterval(upInterval)
 
             //Descendo
@@ -34,20 +34,35 @@ function jump() {
             //Subindo
             position += 20
 
-            dino.style.botton = position + 'px';
+            dino.style.bottom = position + 'px';
         }
     }, 20)
 }
 
 function createCactus(){
     const cactus = document.createElement('div')
-
     let catcusPosition =1000
+    let randomTime = Math.random() * 6000
 
     cactus.classList.add('cactus')
     cactus.style.left = 1000 + 'px'
     background.appendChild(cactus)
+    
+
+    let leftInterval = setInterval(()=>{
+ 
+        if(catcusPosition < -60){
+            clearInterval(leftInterval)
+            background.removeChild(cactus)
+        } else{
+        catcusPosition -= 10
+        cactus.style.left = catcusPosition + 'px'
+        }
+    }, 20)
+    setTimeout(createCactus, randomTime)
 }
+
+
 
 createCactus()
 document.addEventListener('keyup', handleKeyUp)
