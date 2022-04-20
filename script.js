e9d350c64ea8694040261fdc72870f7e962e47e6
Taiwan9 <clownser1994@gmail.com>
2022-04-20 16:44:38 -0300

    cactus has been created

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 const dino = document.querySelector('.dino')
+const background = document.querySelector('.background')
+let isJumping = false
 
-function handleKeyUp(event){
-    if(event.keyCode ===32){
-        jump()
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if (!isJumping) {
+            jump()
+        }
     }
 }
 
-function jump(){
+function jump() {
     let position = 0
-    let upInterval = setInterval(()=>{
-        if(position >=150){
-            clearInterval(upInterval)
-            //Descendo
-            let downInterval = setInterval(()=>{
 
-                if(position<=0){
+    isJumping = true
+
+    let upInterval = setInterval(() => {
+        if (position >= 180) {
+            clearInterval(upInterval)
+
+            //Descendo
+            let downInterval = setInterval(() => {
+
+                if (position <= 0) {
                     clearInterval(downInterval)
-                } else{
-                position -=20
-                dino.style.bottom = position + 'px';
+                    isJumping=false
+                } else {
+                    position -= 20
+                    dino.style.bottom = position + 'px';
                 }
             })
-        }else{
-        //Subindo
-        position +=20
+        } else {
+            //Subindo
+            position += 20
 
-        dino.style.botton = position + 'px';
-    }
-    }, 15)
+            dino.style.botton = position + 'px';
+        }
+    }, 20)
 }
 
-document.addEventListener('keyup',handleKeyUp)
+function createCactus(){
+    const cactus = document.createElement('div')
+
+    let catcusPosition =1000
+
+    cactus.classList.add('cactus')
+    cactus.style.left = 1000 + 'px'
+    background.appendChild(cactus)
+}
+
+createCactus()
+document.addEventListener('keyup', handleKeyUp)
